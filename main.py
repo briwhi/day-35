737f529d8414ac0347c7499c471aac761eb66bf1
Brian Whiteley <brianwhiteleybw@gmail.com>
2020-12-21 15:10:47 -0600

    rain app

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import requests
 
 
-
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 API = "3bdaee74e382c162ea2715c75ee461a5"
 LAT = "38.432232"
 LNG = "-90.378540"
+weather_parameters = {
+    "lat": LAT,
+    "lon": LNG,
+    "units": "imperial",
+    "appid": API,
+    "exclude": "current,minutely,daily,alerts"
+}
+
+response = requests.get(OWM_endpoint, params = weather_parameters)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data['hourly'][:12]
+for hour_data in weather_slice:
+    condition_code = hour_data['weather'][0]["id"]
+    if int(condition_code) < 700:
+        print("rain")
